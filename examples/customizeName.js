7214f66c781548c755ea9158d17682612f6be432
limingmin <314407103@qq.com>
2016-08-16 14:21:35 +0800

    Updates

--- examples/customizeName.js
+++
@@ -1,9 +0,0 @@
-webpackJsonp([10,0],[
-/* 0 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-/***/ }
-]);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZXMvY3VzdG9taXplTmFtZS5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7OzsiLCJzb3VyY2VSb290IjoiIn0=
